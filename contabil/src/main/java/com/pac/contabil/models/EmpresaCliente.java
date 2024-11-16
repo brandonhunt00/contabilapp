2114e76467bf7051dcf1a6b56c69104b42203d0f
vinigbeltrao <vinigbeltrao@gmail.com>
2024-11-16 18:01:31 -0300

    alteracao de dependencias e lombok adicionado

--- contabil/src/main/java/com/pac/contabil/models/EmpresaCliente.java
+++ contabil/src/main/java/com/pac/contabil/models/EmpresaCliente.java
@@ -2,8 +2,10 @@ package com.pac.contabil.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class EmpresaCliente {
 
     @Id
@@ -13,48 +15,4 @@ public class EmpresaCliente {
     private String telefone;
     private String email;
 
-    // Getter e Setter para cnpj
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    // Getter e Setter para razaoSocial
-    public String getRazaoSocial() {
-        return razaoSocial;
-    }
-
-    public void setRazaoSocial(String razaoSocial) {
-        this.razaoSocial = razaoSocial;
-    }
-
-    // Getter e Setter para endereco
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    // Getter e Setter para telefone
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    // Getter e Setter para email
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
