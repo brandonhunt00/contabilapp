6db88ed979e93d4ead20133ddcf1b0fc80d35b86
vinigbeltrao <vinigbeltrao@gmail.com>
2024-11-16 23:07:14 -0300

    mudancas em services e exclusao de repository

--- contabil/src/main/java/com/pac/contabil/controllers/EmpresaClienteController.java
+++ contabil/src/main/java/com/pac/contabil/controllers/EmpresaClienteController.java
@@ -1,7 +1,6 @@
 package com.pac.contabil.controllers;
 
 import com.pac.contabil.models.EmpresaCliente;
-import com.pac.contabil.services.EmpresaClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +10,5 @@ import java.util.List;
 @RequestMapping("/api/empresas")
 public class EmpresaClienteController {
 
-    @Autowired
-    private EmpresaClienteService empresaClienteService;
 
-    @GetMapping
-    public List<EmpresaCliente> getAllEmpresas() {
-        return empresaClienteService.findAll();
-    }
-
-    @PostMapping
-    public EmpresaCliente createEmpresa(@RequestBody EmpresaCliente empresa) {
-        return empresaClienteService.save(empresa);
-    }
 }
--- contabil/src/main/java/com/pac/contabil/repositories/EmpresaClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pac.contabil.repositories;
-
-import com.pac.contabil.models.EmpresaCliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaClienteRepository extends JpaRepository<EmpresaCliente, String> {
-}
